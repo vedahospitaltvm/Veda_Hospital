e6c400f1294015f763c14ad278b03cfe0ef636f4
vedahospitaltvm <110085011+vedahospitaltvm@users.noreply.github.com>
2022-07-27 11:57:58 +0530

    Add files via upload

---
+++ Hospital.js
@@ -0,0 +1,81 @@
+// init Isotope
+var $grid = $('.collection-list').isotope({
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns(){
+  filterBtns.each(function(){
+    $(this).removeClass('active-filter-btn');
+  });
+}
+
+
+
+var tabs = document.querySelectorAll(".tabs ul li");
+var tab_wraps = document.querySelectorAll(".tab_wrap");
+
+tabs.forEach(function(tab, tab_index){
+	tab.addEventListener("click", function(){
+		tabs.forEach(function(tab){
+			tab.classList.remove("active");
+		})
+		tab.classList.add("active");
+
+		tab_wraps.forEach(function(content, content_index){
+			if(content_index == tab_index){
+				content.style.display = "block";
+			}
+			else{
+				content.style.display = "none";
+			}
+		})
+
+	})
+
+})
+
+
+
+const trigger = document.querySelector("menu > .trigger");
+trigger.addEventListener('click', (e) => {
+  e.currentTarget.parentElement.classList.toggle("open");
+});
+
+
+
+
+$(window).on("load resize",function() {
+
+	var counters = $(".count");
+	var countersQuantity = counters.length;
+	var counter = [];
+  
+	for (i = 0; i < countersQuantity; i++) {
+	  counter[i] = parseInt(counters[i].innerHTML);
+	}
+  
+	var count = function(start, value, id) {
+	  var localStart = start;
+	  setInterval(function() {
+		if (localStart < value) {
+		  localStart++;
+		  counters[id].innerHTML = localStart;
+		}
+	  }, 4);
+	}
+  
+	for (j = 0; j < countersQuantity; j++) {
+	  count(0, counter[j], j);
+	}
+  });
+
+
+
+ 
